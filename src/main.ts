9d892bbf6bb13ba1db178cbf0c19b45cd445f759
thư nguyễn <nnmthu08@gmail.com>
2024-02-25 22:07:14 +0700

    khởi tajo1

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+app.useGlobalPipes( new ValidationPipe());
+
+  const config = new DocumentBuilder()
+  .setTitle("Capstone NODE38")
+  .addBearerAuth()
+  .setDescription("Movie Api")
+  .setVersion("1.0")
+  .build()
+
+  const swagger = SwaggerModule.createDocument(app,config);
+  
+  SwaggerModule.setup("swagger",app,swagger)
+
+  await app.listen(3000);
+}
+bootstrap();
